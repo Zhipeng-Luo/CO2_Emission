2d72660c2483fb8c9d83268bc35ef296ecc57e3a
ZHIPENG LUO <66469893+Zhipeng-Luo@users.noreply.github.com>
2021-12-21 19:53:09 +0800

    delete column4

--- Global_warm.py
+++ Global_warm.py
@@ -78,13 +78,6 @@ with col3:
     ))
     st.plotly_chart(fig2, use_container_width=True)
     
-with col4:
-    df_climate = load_data_climate().query("Entity == 'World' and Year <=2021")
-    fig3 = px.scatter(df_climate, x="Year", y="annual_sea_surface_temperature_anomaly", \
-                    trendline='lowess', title = 'Sea Surface Temperature Anomaly')
-    fig3.update_yaxes(title = 'Degree Celsius')
-
-    st.plotly_chart(fig3, use_container_width=True)
 
 st.header('CO2 Per Capita')
 col5, space3, col6 = st.columns([9,1,9])
